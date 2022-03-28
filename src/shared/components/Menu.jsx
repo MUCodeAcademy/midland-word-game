d0c649df627481162bf4ee918b0f41e5ce168f8b
Mike Truax <mike@miketruax.com>
2022-03-28 16:07:07 -0500

    adds menu boilerplate

---
+++ src/shared/components/Menu.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { connect } from "react-redux";
+
+export const Menu = (props) => {
+  return <div>Menu</div>;
+};
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Menu);
