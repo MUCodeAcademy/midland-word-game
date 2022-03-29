8892c0ce72b23894325bb8ad3e8714b539de2a3f
Sydney Havengar <sydney.havengar01@gmail.com>
2022-03-29 12:31:14 -0500

    adds mui drawer for menu

--- src/shared/components/Menu.jsx
+++ src/shared/components/Menu.jsx
@@ -1,38 +1,69 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { NavLink } from "react-router-dom";
+import {
+  Box,
+  Drawer,
+  Button,
+  List,
+  Divider,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
 
 export const Menu = () => {
   const [battleOptions, setBattleOptions] = useState(false);
-
+  const [user, setUser] = useState(null);
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [isActive] = useState();
   const battleClick = () => {
     setBattleOptions(true);
   };
+
   return (
-    <nav>
+    <>
       {!user && (
-        <div>
-          <NavLink to="register">Register</NavLink>
-          <NavLink to="login">Login</NavLink>
-        </div>
+        <>
+          <NavLink
+            to="register"
+            className={({ isActive }) => (isActive ? "activeLink" : "link")}
+          >
+            Register
+          </NavLink>
+          <NavLink
+            to="login"
+            className={({ isActive }) => (isActive ? "activeLink" : "link")}
+          >
+            Login
+          </NavLink>
+        </>
       )}
       {user && (
-        <div>
-          <NavLink to="about">About</NavLink>
-          <NavLink to="classic">Classic</NavLink>
-          <NavLink to="play" onClick={battleClick}>
-            Battle
-          </NavLink>
-          {battleOptions && (
-            <div>
-              <NavLink to="create">Create Battle</NavLink>
-              <NavLink to="join">Join Battle</NavLink>
-            </div>
-          )}
-          <NavLink to="login">Logout</NavLink>
-        </div>
+        <>
+          <Button onClick={() => setDrawerOpen(true)}>Menu</Button>
+          <Drawer
+            anchor="left"
+            open={drawerOpen}
+            onClose={() => setDrawerOpen(false)}
+          >
+            <Box width=" 250px" textAlign="center" role="presentation">
+              <NavLink to="about">About</NavLink>
+              <NavLink to="classic">Classic</NavLink>
+              <NavLink to="play" onClick={battleClick}>
+                Battle
+              </NavLink>
+              {battleOptions && (
+                <div>
+                  <NavLink to="create">Create Battle</NavLink>
+                  <NavLink to="join">Join Battle</NavLink>
+                </div>
+              )}
+              <NavLink to="login">Logout</NavLink>
+            </Box>
+          </Drawer>
+        </>
       )}
-    </nav>
+    </>
   );
 };
 
