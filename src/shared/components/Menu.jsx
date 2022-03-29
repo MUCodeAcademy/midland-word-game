bd57be54e332d4f8100292900b3099cc560419fe
Sydney Havengar <sydney.havengar01@gmail.com>
2022-03-29 11:08:14 -0500

    adds conditional links for user and battle options

--- src/shared/components/Menu.jsx
+++ src/shared/components/Menu.jsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
+import { NavLink } from "react-router-dom";
 
-export const Menu = (props) => {
-  return <div>Menu</div>;
+export const Menu = () => {
+  const [battleOptions, setBattleOptions] = useState(false);
+
+  const battleClick = () => {
+    setBattleOptions(true);
+  };
+  return (
+    <nav>
+      {!user && (
+        <div>
+          <NavLink to="register">Register</NavLink>
+          <NavLink to="login">Login</NavLink>
+        </div>
+      )}
+      {user && (
+        <div>
+          <NavLink to="about">About</NavLink>
+          <NavLink to="classic">Classic</NavLink>
+          <NavLink to="play" onClick={battleClick}>
+            Battle
+          </NavLink>
+          {battleOptions && (
+            <div>
+              <NavLink to="create">Create Battle</NavLink>
+              <NavLink to="join">Join Battle</NavLink>
+            </div>
+          )}
+          <NavLink to="login">Logout</NavLink>
+        </div>
+      )}
+    </nav>
+  );
 };
 
 const mapStateToProps = (state) => ({});
