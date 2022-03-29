31f51415cc0fd740dc2abfdd2b9f829aea85f2d4
Brett Ellis <brett.ash.ellis@gmail.com>
2022-03-29 09:39:38 -0500

    User Routes done

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,4 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const { login, register } = require("../models/user.models")
+
+router.post("/login", (req, res) => {
+    const { username, password } = req.body;
+    login(res, username, password);
+});
+
+router.put("/register", (req, res) => {
+    console.log(req.body, "Console log for Register Routes! Things are all good here!")
+    register(res, req.body.username, req.body.password);
+});
+
+router.get("/logout", (req, res) => {
+    //! May need to adjust "access_token"
+    res.clearCookie("access_token");
+    return res.send({
+        data: null,
+        success: true,
+        error: null,
+    });
+});
+
+router.get("/verify", (req, res) => {
+    return res.send({
+        data: { username: req.user.username },
+        success: true,
+        error: null
+    });
+});
 
 module.exports = router;
