952966c853b403f37730896d986189eb1626b1e5
Melrov <rdunham918@gmail.com>
2022-03-29 09:35:25 -0500

    creates authenticate middleware

--- server/middleware/authenticate.middleware.js
+++ server/middleware/authenticate.middleware.js
@@ -1,5 +1,14 @@
-function authenticate(req, res, next) {
-  return next();
+const passport = require("passport");
+
+async function authenticate(req, res, next) {
+  await passport.authenticate("jwt", (err, user, info) => {
+    if (err || !user) {
+      res.clearCookie("access_token");
+      return res.send({ success: false, data: null, error: info });
+    }
+    req.user = user;
+    return next();
+  })(req, res, next);
 }
 
 module.exports = authenticate;
