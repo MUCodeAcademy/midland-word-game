52b241e5eba55545156aeb6278a81f54d4722c89
Thomas.Schumacher <Tommyschumacher@gmail.com>
2022-03-29 16:36:27 -0500

    adds routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,90 @@
 import "./App.css";
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+import AboutPage from "./components/AboutPage";
+import ClassicPage from "./components/ClassicPage";
+import LoginPage from "./components/LoginPage";
+import PlayPage from "./components/PlayPage";
+import RegisterPage from "./components/RegisterPage";
+import Menu from "./shared/components/Menu";
+import ProtectedRoute from "./shared/components/ProtectedRoute";
+import { connect } from "react-redux";
+import { setUser } from "./redux/actions/user.actions";
+import { verify } from "./shared/hooks/useAPI";
+import useAPI from "./shared/hooks/useAPI";
+import { useEffect } from "react";
 
-function App() {
-  return <div></div>;
+
+
+
+function App({user, setUser}) {
+  const {verify} = useAPI();
+
+  useEffect(() => {
+    const verifyUser = async () => {
+      const res = await verify();
+        if(res.data.success){
+          setUser(res.data.username)
+        } 
+    }; verifyUser()
+  }, []);
+
+    return (<Router>
+      <Menu />
+      <Routes>
+        <Route 
+          path="/Play"
+          element={
+            <ProtectedRoute isPrivate={true}>
+              <PlayPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route 
+          path="/login"
+          element={
+            <ProtectedRoute isPrivate={false}>
+              <LoginPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route 
+          path="/about"
+          element={
+            <ProtectedRoute isPrivate={false}>
+              <AboutPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route 
+          path="/classic"
+          element={
+            <ProtectedRoute isPrivate={true}>
+              <ClassicPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route 
+          path="/register"
+          element={
+            <ProtectedRoute isPrivate={false}>
+              <RegisterPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route 
+          path="*"
+          element={<Navigate to="/login" />} />
+           
+      </Routes>
+    </Router>)
 }
 
-export default App;
+const mapStateToProps = (state) => {};
+const mapDispatchToProps = (setUser) => {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
