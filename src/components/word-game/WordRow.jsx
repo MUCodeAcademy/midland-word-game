e8e62011e4510d4f69f7b326488581476a37b9d7
Sophie Newell <sophienewellstudio@gmail.com>
2022-03-30 09:08:09 -0500

    adds basic structure

--- src/components/word-game/WordRow.jsx
+++ src/components/word-game/WordRow.jsx
@@ -1,7 +1,79 @@
-import React from "react";
+import { Grid, Item } from "@mui/material";
+import React, { useState } from "react";
 
-function WordRow() {
-  return <div>WordRow</div>;
+function WordRow({ guess, currentWord }) {
+  const [correct, setCorrect] = useState(false);
+  const [letterIncluded, setLetterIncluded] = useState(false);
+
+  const guessArr = guess.split("");
+  const currWordArr = currentWord.split("");
+
+  return (
+    <div>
+      <Grid container spacing={1} columns={10}>
+        <Grid item xs={2}>
+          <Item>
+            <div
+              id="letter1"
+              className={`${
+                correct ? "correct" : letterIncluded ? "included" : "incorrect"
+              }`}
+            >
+              {guess[0]}
+            </div>
+          </Item>
+        </Grid>
+        <Grid item xs={2}>
+          <Item>
+            <div
+              id="letter2"
+              className={`${
+                correct ? "correct" : letterIncluded ? "included" : "incorrect"
+              }`}
+            >
+              {guess[1]}
+            </div>
+          </Item>
+        </Grid>
+        <Grid item xs={2}>
+          <Item>
+            <div
+              id="letter3"
+              className={`${
+                correct ? "correct" : letterIncluded ? "included" : "incorrect"
+              }`}
+            >
+              {guess[2]}
+            </div>
+          </Item>
+        </Grid>
+        <Grid item xs={2}>
+          <Item>
+            <div
+              id="letter4"
+              className={`${
+                correct ? "correct" : letterIncluded ? "included" : "incorrect"
+              }`}
+            >
+              {guess[3]}
+            </div>
+          </Item>
+        </Grid>
+        <Grid item xs={2}>
+          <Item>
+            <div
+              id="letter5"
+              className={`${
+                correct ? "correct" : letterIncluded ? "included" : "incorrect"
+              }`}
+            >
+              {guess[4]}
+            </div>
+          </Item>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default WordRow;
