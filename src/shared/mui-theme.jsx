f9edd3bb8fd3dccecdd7d3c0bb8b59d23cdb8e5f
Sydney Havengar <sydney.havengar01@gmail.com>
2022-04-06 09:21:35 -0500

    removes button styling (didn't work)

--- src/shared/mui-theme.jsx
+++ src/shared/mui-theme.jsx
@@ -1,35 +1,20 @@
-import { createTheme } from "@mui/material/styles"
+import { createTheme } from "@mui/material/styles";
 
 export const generalTheme = createTheme({
-    palette: {
-        type: 'light',
-        primary: {
-            main: '#2E4057',
-        },
-        secondary: {
-            main: '#FF934F',
-        },
-        info: {
-            main: '#2196f3',
-        },
-        background: {
-            default: '#FAF8D4',
-            paper: 'rgb(87, 102, 120)',
-        },
+  palette: {
+    type: "light",
+    primary: {
+      main: "#2E4057",
     },
-    button: {
-        "&:active": {
-            boxShadow: "none",
-            color: "secondary",
-            variant: "outlined",
-        },
-        color: "secondary",
-        variant: "contained",
+    secondary: {
+      main: "#FF934F",
     },
-})
-//ahhh okay, Ill take a look at the link lol HAHA
-// damn, i knew they were vanilla
-//https://stackoverflow.com/questions/40591095/how-to-change-background-color-of-a-selected-itemlist-material-ui
-// Might have to create a theme with some OnHovers
-// 
-//
+    info: {
+      main: "#2196f3",
+    },
+    background: {
+      default: "#FAF8D4",
+      paper: "rgb(87, 102, 120)",
+    },
+  },
+});
