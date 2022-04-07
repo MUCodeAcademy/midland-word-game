6019af4ff42bf9895036cc6f2da574381b458daf
Sophie Newell <sophienewellstudio@gmail.com>
2022-04-07 12:41:39 -0500

    styling for Game Page

--- src/components/game-room/GamePage.jsx
+++ src/components/game-room/GamePage.jsx
@@ -5,6 +5,9 @@ import useSocket from "../../shared/hooks/useSocket";
 import WordBoard from "../word-game/WordBoard";
 import Clock from "./Clock";
 import Score from "./Score";
+import { Button, ThemeProvider, Grid } from "@mui/material/";
+import generalTheme from "../../shared/mui-theme";
+import { Box } from "@mui/system";
 
 export const GamePage = () => {
   const copyBtn = useRef();
@@ -32,46 +35,82 @@ export const GamePage = () => {
   }, [joinRoom, roomId]);
 
   return (
-    <div>
-      <div>{(error || roomMessage) && <span>{error ? error : roomMessage}</span>}</div>
-      <div>
-        <div>
-          <div>
-            <span>{roomId}</span>
-            <button
-              ref={copyBtn}
-              onClick={() => {
-                navigator.clipboard.writeText(roomId);
-                copyBtn.current.innerText = "Copied";
-              }}
-            >
-              Copy
-            </button>
-          </div>
-          {isHost && (
-            <>
-              {!runningGame && <button onClick={() => startGame()}>Start Game</button>}
-              {runningGame && !runningRound && <button onClick={() => startRound()}>Start Round</button>}
-            </>
-          )}
+    <ThemeProvider theme={generalTheme}>
+      <div className="center">
+        <div className="padding-10">
+          {(error || roomMessage) && <span>{error ? error : roomMessage}</span>}
         </div>
         <div>
-          <Clock roomTimer={roomTimer} />
-          <WordBoard
-            submitWord={submitWord}
-            guesses={guesses}
-            roundWord={roundWord}
-            runningGame={runningGame}
-            runningRound={runningRound}
-            playerWonRound={playerWonRound}
-            solo={false}
-          />
-        </div>
-        <div className="score-container">
-          <Score players={players} username={username} runningRound={runningRound} />
+          <Grid container spacing={4} justifyContent="center" display="flex">
+            <Grid item xs={4}>
+              <div>
+                <span className="margin-10">Room ID: {roomId}</span>
+                <Button
+                  variant="contained"
+                  ref={copyBtn}
+                  onClick={() => {
+                    navigator.clipboard.writeText(roomId);
+                    copyBtn.current.innerText = "Copied";
+                  }}
+                >
+                  Copy
+                </Button>
+              </div>
+            </Grid>
+            <Grid item xs={4}>
+              {isHost && (
+                <>
+                  {!runningGame && (
+                    <Button variant="contained" onClick={() => startGame()}>
+                      Start Game
+                    </Button>
+                  )}
+                  {runningGame && !runningRound && (
+                    <Button variant="contained" onClick={() => startRound()}>
+                      Start Round
+                    </Button>
+                  )}
+                </>
+              )}
+              {!runningGame && roundWord && (
+                <div className="padding-10">
+                  <span>{`The word was ${roundWord}`}</span>
+                </div>
+              )}
+            </Grid>
+            <Grid item xs={4}>
+              <div>
+                <Clock roomTimer={roomTimer} />
+              </div>
+            </Grid>
+            <Grid item xs={12}>
+              <div className="width-80pc">
+                <Box display="flex" justifyContent="center">
+                  <WordBoard
+                    submitWord={submitWord}
+                    guesses={guesses}
+                    roundWord={roundWord}
+                    runningGame={runningGame}
+                    runningRound={runningRound}
+                    playerWonRound={playerWonRound}
+                    solo={false}
+                  />
+                </Box>
+              </div>
+            </Grid>
+            <Grid item xs={12}>
+              <div className="score-container margin-auto">
+                <Score
+                  players={players}
+                  username={username}
+                  runningRound={runningRound}
+                />
+              </div>
+            </Grid>
+          </Grid>
         </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 };
 
---
+++ src/shared/mui-theme.jsx
@@ -0,0 +1,26 @@
+import { createTheme } from "@mui/material/styles";
+
+const generalTheme = createTheme({
+  palette: {
+    type: "light",
+    primary: {
+      //dark blue
+      main: "#2E4057",
+    },
+    secondary: {
+      //orange
+      main: "#FF934F",
+    },
+    info: {
+      //medium blue
+      main: "#2196f3",
+    },
+    background: {
+      //pale yellow
+      default: "#FAF8D4",
+
+      paper: "rgb(87, 102, 120)",
+    },
+  },
+});
+export default generalTheme;
