40969f96728d72a5ba6fd39937ccb9276b156d92
Mike Truax <mike@miketruax.com>
2022-04-08 10:05:50 -0500

    updates menu layout

--- src/shared/components/Menu.jsx
+++ src/shared/components/Menu.jsx
@@ -6,114 +6,145 @@ import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
 import PersonIcon from "@mui/icons-material/Person";
 import LogoutIcon from "@mui/icons-material/Logout";
 import InfoIcon from "@mui/icons-material/Info";
-import { clearUser } from "../../redux/actions"
+import LoginIcon from "@mui/icons-material/Login";
+import RegisterIcon from "@mui/icons-material/PersonAdd";
+import { clearUser } from "../../redux/actions";
 import {
   AppBar,
-  Toolbar,
-  Button,
   Box,
-  Drawer,
+  SwipeableDrawer,
   IconButton,
   Divider,
   List,
   ListItem,
   ListItemText,
   ListItemIcon,
-  ButtonGroup,
+  Avatar,
+  Toolbar,
 } from "@mui/material";
 import useAPI from "../hooks/useAPI";
 
 export const Menu = ({ user, clearUser }) => {
   const [drawerOpen, setDrawerOpen] = useState(false);
-  const { logout: logoutApi } = useAPI()
+  const { logout: logoutApi } = useAPI();
 
-  const logout = useCallback(async() => {
-    const res = await logoutApi()
-    if(res.success){
-      clearUser()
-      setDrawerOpen(false)
+  const logout = useCallback(async () => {
+    const res = await logoutApi();
+    if (res.success) {
+      clearUser();
+      setDrawerOpen(false);
     }
-  }, [logoutApi, clearUser, setDrawerOpen])
+  }, [logoutApi, clearUser, setDrawerOpen]);
 
   return (
     <>
-      {!user && (
-        <>
-          <AppBar position="static">
-            <Box display="flex" justifyContent="center" alignItems="center">
-              <Toolbar>
-                <ButtonGroup color="secondary" variant="contained">
-                  <Button>
-                    <NavLink to="register" className="noTextDecor">
-                      REGISTER
-                    </NavLink>
-                  </Button>
-                  <Button>
-                    <NavLink to="login" className="noTextDecor">
-                      LOGIN
-                    </NavLink>
-                  </Button>
-                  <Button>
-                    <NavLink to="about" className="noTextDecor">
-                      ABOUT
-                    </NavLink>
-                  </Button>
-                </ButtonGroup>
-              </Toolbar>
-            </Box>
-          </AppBar>
-        </>
-      )}
-      {user && (
-        <>
-          <IconButton size="large" onClick={() => setDrawerOpen(true)}>
+      <AppBar position="static">
+        <Toolbar className="toolbar">
+          <img
+            className="menu-icon"
+            alt="word-battle-icon"
+            src="./word-game-logo.png"
+          />
+          <IconButton
+            style={{ alignSelf: "flex-end", color: "#faf8d4" }}
+            size="large"
+            onClick={() => setDrawerOpen(true)}
+          >
             <MenuIcon />
           </IconButton>
-          <Drawer
-            anchor="left"
-            open={drawerOpen}
-            onClose={() => setDrawerOpen(false)}
-          >
-            <Box width=" 250px">
-              <List>
-                <NavLink to="classic" className="noTextDecor" onClick={() => setDrawerOpen(false)}>
-                  <ListItem button>
+        </Toolbar>
+      </AppBar>
+      <SwipeableDrawer
+        anchor="left"
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+      >
+        <Box width=" 250px">
+          <List>
+            {!user && (
+              <>
+                <NavLink
+                  to="register"
+                  className="menu-link"
+                  onClick={() => setDrawerOpen(false)}
+                >
+                  <ListItem button className="menu-item">
+                    <ListItemIcon>
+                      <RegisterIcon />
+                    </ListItemIcon>
+                    <ListItemText>Register</ListItemText>
+                  </ListItem>
+                </NavLink>
+                <NavLink
+                  to="login"
+                  className="menu-link"
+                  onClick={() => setDrawerOpen(false)}
+                >
+                  <ListItem button className="menu-item">
+                    <ListItemIcon>
+                      <LoginIcon />
+                    </ListItemIcon>
+                    <ListItemText>Login</ListItemText>
+                  </ListItem>
+                </NavLink>
+              </>
+            )}
+            {user && (
+              <>
+                <NavLink
+                  to="classic"
+                  className="menu-link"
+                  onClick={() => setDrawerOpen(false)}
+                >
+                  <ListItem button className="menu-item">
                     <ListItemIcon>
                       <PersonIcon />
                     </ListItemIcon>
-                    <ListItemText>CLASSIC</ListItemText>
+                    <ListItemText>Classic</ListItemText>
                   </ListItem>
                 </NavLink>
-                <NavLink to="play" className="noTextDecor" onClick={() => setDrawerOpen(false)}>
-                  <ListItem button>
+                <NavLink
+                  to="play"
+                  className="menu-link"
+                  onClick={() => setDrawerOpen(false)}
+                >
+                  <ListItem button className="menu-item">
                     <ListItemIcon>
                       <PeopleAltIcon />
                     </ListItemIcon>
-                    <ListItemText>BATTLE</ListItemText>
+                    <ListItemText>Battle</ListItemText>
                   </ListItem>
                 </NavLink>
-                <NavLink to="about" className="noTextDecor" onClick={() => setDrawerOpen(false)}>
-                  <ListItem button>
-                    <ListItemIcon>
-                      <InfoIcon />
-                    </ListItemIcon>
-                    <ListItemText>ABOUT</ListItemText>
-                  </ListItem>
-                </NavLink>
-                <Divider />
-                <NavLink to="" className="noTextDecor" onClick={() => logout()}>
-                  <ListItem button>
+              </>
+            )}
+            <NavLink
+              to="about"
+              className="menu-link"
+              onClick={() => setDrawerOpen(false)}
+            >
+              <ListItem button className="menu-item">
+                <ListItemIcon>
+                  <InfoIcon />
+                </ListItemIcon>
+                <ListItemText>About</ListItemText>
+              </ListItem>
+            </NavLink>
+            <Divider />
+            {user && (
+              <>
+                <NavLink to="" className="menu-link" onClick={() => logout()}>
+                  <ListItem button className="menu-item">
                     <ListItemIcon>
                       <LogoutIcon />
                     </ListItemIcon>
-                    <ListItemText>LOGOUT</ListItemText>
+                    <ListItemText>Logout</ListItemText>
                   </ListItem>
                 </NavLink>
-              </List>
-            </Box>
-          </Drawer>
-        </>
-      )}
+              </>
+            )}
+          </List>
+        </Box>
+      </SwipeableDrawer>
     </>
   );
 };
