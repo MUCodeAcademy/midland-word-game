a5c22776d49a681e040edfcbf1cc3b5baa506ea9
Sophie Newell <sophienewellstudio@gmail.com>
2022-04-08 14:07:31 -0500

    removes input field and autopopulates boxes

--- src/components/word-game/WordBoard.jsx
+++ src/components/word-game/WordBoard.jsx
@@ -3,7 +3,6 @@ import { connect } from "react-redux";
 import WordKeyboard from "./WordKeyboard";
 import WordRow from "./WordRow";
 
-
 export const WordBoard = ({
   submitWord,
   guesses,
@@ -16,6 +15,52 @@ export const WordBoard = ({
   const [guessedLetters, setGuessedLetters] = useState({});
   const [guessesObjs, setGuessesObjs] = useState([]);
 
+  const [inputGuess, setInputGuess] = useState([]);
+  const [guessFill, setGuessFill] = useState([]);
+
+  //KeyDown event listener on entire page
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+    return function cleanup() {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  const handleKeyDown = useCallback(
+    (e) => {
+      console.log(runningRound);
+      if (!playerWonRound) {
+        //enter
+        if (e.keyCode === 13) {
+          console.log(inputGuess);
+          submitWord(inputGuess.join(""));
+        }
+        //backspace
+        else if (e.keyCode === 8) {
+          setInputGuess((curr) => [...curr.pop()]);
+        }
+        //letters
+        else if (e.keyCode > 64 && e.keyCode < 91) {
+          if (inputGuess.length < 5) {
+            setInputGuess((curr) => [...curr, e.key]);
+          }
+        }
+      }
+    },
+    [runningRound, inputGuess]
+  );
+
+  //sets inputGuess into correct format for WordRowDisplay
+  useEffect(() => {
+    console.log(inputGuess);
+    setGuessFill([
+      { letter: inputGuess[0] ? inputGuess[0] : "‎", status: "" },
+      { letter: inputGuess[1] ? inputGuess[1] : "‎", status: "" },
+      { letter: inputGuess[2] ? inputGuess[2] : "‎", status: "" },
+      { letter: inputGuess[3] ? inputGuess[3] : "‎", status: "" },
+      { letter: inputGuess[4] ? inputGuess[4] : "‎", status: "" },
+    ]);
+  }, [inputGuess]);
 
   const letterDistCheck = useCallback(
     (word) => {
@@ -67,14 +112,13 @@ export const WordBoard = ({
     });
   }, [guesses, setGuessesObjs, checkGuess]);
 
-useEffect(() => {
-  if(runningRound){
-    setGuessedLetters({})
-  }
-}, [runningRound])
+  useEffect(() => {
+    if (runningRound) {
+      setGuessedLetters({});
+    }
+  }, [runningRound]);
 
   return (
-
     <div className="border">
       <div className="padding-10">
         {
@@ -84,15 +128,16 @@ useEffect(() => {
             runningGame={runningGame}
             runningRound={runningRound}
             playerWonRound={playerWonRound}
+            guessFill={guessFill}
           />
         }
       </div>
+
       <div className="padding-10">
         {(!solo || guesses.length < 6) && (
           <WordKeyboard guessedLetters={guessedLetters} />
         )}
       </div>
-
     </div>
   );
 };
--- src/components/word-game/WordRow.jsx
+++ src/components/word-game/WordRow.jsx
@@ -3,32 +3,21 @@ import { Box } from "@mui/system";
 import React, { useEffect, useRef, useCallback, useState } from "react";
 import WordRowDisplay from "./WordRowDisplay";
 
-function WordRow({ runningRound, playerWonRound, guesses, submitWord, solo }) {
-  const guessInput = useRef(null);
+function WordRow({
+  runningRound,
+  playerWonRound,
+  guesses,
+  solo,
+  guessFill,
+  inputGuess,
+}) {
   const [guessesFill, setGuessesFill] = useState([]);
 
-  const [inputGuess, setInputGuess] = useState([]);
-
-  const handleKeyPress = (event) => {
-    setInputGuess((curr) => [...curr, [event.key]]);
-  };
-
-  //autofocus on input after each submission
-  function resetForm() {
-    guessInput.current.focus();
-  }
-
-  const handleClick = useCallback(
-    (word) => {
-      submitWord(word);
-      resetForm();
-    },
-    [submitWord]
-  );
+  const [guessedWord, setGuessedWord] = useState("");
 
   useEffect(() => {
-    if (guessInput.current) {
-      guessInput.current.value = "";
+    if (guessedWord) {
+      setGuessedWord("");
     }
     setGuessesFill([]);
     for (let i = 0; i < 6 - guesses.length; i++) {
@@ -57,31 +46,11 @@ function WordRow({ runningRound, playerWonRound, guesses, submitWord, solo }) {
             guessesFill.map((val, i) => <WordRowDisplay key={i} val={val} />)}
         </div>
       )}
-
-      {runningRound && !playerWonRound && (
-        <div>
-          {inputGuess}
-          <input
-            id="input"
-            type="text"
-            ref={guessInput}
-            placeholder="type your guess here"
-            onKeyDown={(e) => handleKeyPress(e)}
-            className="margin-10"
-            autoFocus
-          />
-          <Button
-            variant="contained"
-            onClick={() => {
-              handleClick(guessInput.current.value);
-            }}
-          >
-            Submit
-          </Button>
-        </div>
-      )}
+      <div>
+        {console.log(guessFill)}
+        {guessFill && <WordRowDisplay val={guessFill} />}
+      </div>
     </div>
   );
 }
-
 export default WordRow;
