0d902a75a1b8e506da827b3a7eca52da549c9fb6
Melrov <rdunham918@gmail.com>
2022-04-08 15:15:21 -0500

    fixes error last commit added

--- src/components/word-game/WordRow.jsx
+++ src/components/word-game/WordRow.jsx
@@ -29,7 +29,7 @@ function WordRow({
       setGuessedWord("");
     }
     setGuessesFill([]);
-    for (let i = 0; i < (!runningRound ? 6 : 5) - guesses.length; i++) {
+    for (let i = 0; i < 5 - guesses.length; i++) {
       setGuessesFill((curr) => [
         ...curr,
         [
@@ -41,27 +41,35 @@ function WordRow({
         ],
       ]);
     }
-  }, [guesses]);
+  }, [guesses, guessedWord]);
 
   return (
     <div>
-      <Box sx={{ height: solo ? "auto" : 250, overflowY: solo ? "" : "scroll" }}>
+      <Box
+        sx={{ height: solo ? "auto" : 250, overflowY: solo ? "" : "scroll" }}
+      >
         {guesses &&
           guesses.map((val, i) => <WordRowDisplay key={i} val={val} />)}
-      {solo && (
-        <div>
-          {guessesFill &&
-            guessesFill.map((val, i) => <WordRowDisplay key={i} val={val} />)}
-        </div>
-      )}
+        {solo && (
+          <div>
+            {guessesFill &&
+              guessesFill.map((val, i) => <WordRowDisplay key={i} val={val} />)}
+          </div>
+        )}
         <div ref={messagesEndRef} />
       </Box>
-      { !solo &&  <div>
-        {guessFill && runningRound && <WordRowDisplay val={guessFill} />}
-      </div>}
-      { solo &&  <div>
-        {guessFill && guesses.length !== 6 && <WordRowDisplay val={guessFill} />}
-      </div>}
+      {!solo && (
+        <div>
+          {guessFill && runningRound && <WordRowDisplay val={guessFill} />}
+        </div>
+      )}
+      {solo && (
+        <div>
+          {guessFill && guesses.length !== 6 && (
+            <WordRowDisplay val={guessFill} />
+          )}
+        </div>
+      )}
     </div>
   );
 }
